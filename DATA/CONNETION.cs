0234378781f4ae541c35700be2e13d1c4bbb4356
INAPOLI17 <apolinarar72@gmail.com>
2023-01-07 09:41:06 -0400

    First work 7/1/2023
    Morning

--- DATA/CONNETION.cs
+++ DATA/CONNETION.cs
@@ -1,6 +1,32 @@
-﻿
-namespace DATABASE {
+﻿using DATABASE.LIBRARY.CLADATOS;
+
+namespace DATABASE  {
     public class CONNETION : CheckSession {
+
+        private DATOAGENDA datoAgenda;
+        
+        private DATOSCLIENTE datoCLIENTE;
+        
+        private DATOSDETALLE datoDETALLE;
+        
+        private DATOSFACTURA datoFACTURA ;
+        
+        private DATOSPRODUCTOS datOPRODUCTOS;
+        
+        private DATOSUSER datoUSER;
+
+        public CONNETION ( ) { 
+
+            this.datoAgenda = new DATOAGENDA();
+            this.datoCLIENTE = new DATOSCLIENTE();
+            this.datoDETALLE = new DATOSDETALLE();
+            this.datoFACTURA = new DATOSFACTURA();
+            this.datOPRODUCTOS = new DATOSPRODUCTOS ();
+            this.datoUSER = new DATOSUSER();
+            
+            }
+
+     
 
 
 
--- Lógica/LOGICA.cs
+++ Lógica/LOGICA.cs
@@ -1,25 +1,33 @@
 ﻿using System.Collections.Generic;
 using System.Windows.Forms;
 using DATABASE;
+using DATABASE.LIBRARY.CLADATOS;
 using Lógica.LIBRARY;
 
-namespace LOGIC.LIBRARY {
+
+namespace LOGIC {
     public class LOGICA:Report {
 
         private List<object> listTextBox1;
         private List<object> listRadio1;
         private DataGridView DGVPRINTER;
-        private CONNETION BridgeToData = new CONNETION();
+        private CONNETION brigdetodata = new CONNETION();
+        
 
         public LOGICA ( List<object> listTextBox1, List<object> listRadio1, DataGridView RW ) {
             this.listTextBox1 = listTextBox1;
             this.listRadio1 = listRadio1;
             this.DGVPRINTER = RW;
-            }
+        }
 
         private void Printer ( ) {
 
-            }
+        }
+
+        private void GenerateBill ( List<object> DataForBill) {
+            DATOSFACTURA fa = new DATOSFACTURA();
+
+        }
 
     }
 }
